27e45037e7039a61257895e11cffaf37471bee33
OmerAlali2020 <omeralali@gmail.com>
2023-06-04 15:29:03 +0300

    Add validation message to edit file

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,6 +5,9 @@
                 </h2>
             </x-slot>
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            @if($errors->any())
+    {!! implode('', $errors->all('<div>:message</div>')) !!}
+@endif
                 <form method="POST" action="{{ route('states.update', $state) }}">
                     @csrf
                     @method('patch')
@@ -13,9 +16,7 @@
                         class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                         maxlength="30"
                     >{{ old('name', $state->name) }}</textarea>
-                    @if($errors->has('firstname'))
-    <div class="error">{{ $errors->first('name') }}</div>
-@endif
+                    
                     <textarea
                         name="iso"
                         class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
